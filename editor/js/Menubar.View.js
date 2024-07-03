82bdc8bddf24c3ba3adcc1fc64b972dbfcc7858c
Nik Kovitz <nik@4threal.com>
2024-07-03 13:38:40 -0500

    Omitted navigator.xr.isSessionSupported('immersive-ar') because it keeps throwing exceptions

--- editor/js/Menubar.View.js
+++ editor/js/Menubar.View.js
@@ -1,110 +1,80 @@
 import { UIPanel, UIRow } from './libs/ui.js';
 
-function MenubarView( editor ) {
+function MenubarView(editor) {
 
 	const signals = editor.signals;
 	const strings = editor.strings;
 
 	const container = new UIPanel();
-	container.setClass( 'menu' );
+	container.setClass('menu');
 
 	const title = new UIPanel();
-	title.setClass( 'title' );
-	title.setTextContent( strings.getKey( 'menubar/view' ) );
-	container.add( title );
+	title.setClass('title');
+	title.setTextContent(strings.getKey('menubar/view'));
+	container.add(title);
 
 	const options = new UIPanel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass('options');
+	container.add(options);
 
 	// Fullscreen
-
 	const option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/view/fullscreen' ) );
-	option.onClick( function () {
-
-		if ( document.fullscreenElement === null ) {
-
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/view/fullscreen'));
+	option.onClick(function () {
+		if (document.fullscreenElement === null) {
 			document.documentElement.requestFullscreen();
-
-		} else if ( document.exitFullscreen ) {
-
+		} else if (document.exitFullscreen) {
 			document.exitFullscreen();
-
 		}
 
 		// Safari
-
-		if ( document.webkitFullscreenElement === null ) {
-
+		if (document.webkitFullscreenElement === null) {
 			document.documentElement.webkitRequestFullscreen();
-
-		} else if ( document.webkitExitFullscreen ) {
-
+		} else if (document.webkitExitFullscreen) {
 			document.webkitExitFullscreen();
-
 		}
-
-	} );
-	options.add( option );
+	});
+	options.add(option);
 
 	// XR (Work in progress)
 
-	if ( 'xr' in navigator ) {
-
-		if ( 'offerSession' in navigator.xr ) {
-
-			signals.offerXR.dispatch( 'immersive-ar' );
-
+	if ('xr' in navigator) {
+		if ('offerSession' in navigator.xr) {
+			signals.offerXR.dispatch('immersive-ar');
 		} else {
+			console.log('Skipping xr.isSessionSupported("immersive-ar") to avoid exception');
 
-			navigator.xr.isSessionSupported( 'immersive-ar' )
-				.then( function ( supported ) {
-
-					if ( supported ) {
-
-						const option = new UIRow();
-						option.setClass( 'option' );
-						option.setTextContent( 'AR' );
-						option.onClick( function () {
-
-							signals.enterXR.dispatch( 'immersive-ar' );
-
-						} );
-						options.add( option );
-
-					} else {
-
-						navigator.xr.isSessionSupported( 'immersive-vr' )
-							.then( function ( supported ) {
-
-								if ( supported ) {
-
-									const option = new UIRow();
-									option.setClass( 'option' );
-									option.setTextContent( 'VR' );
-									option.onClick( function () {
-
-										signals.enterXR.dispatch( 'immersive-vr' );
-
-									} );
-									options.add( option );
-
-								}
-
-							} );
-
-					}
-
-			} );
+			// navigator.xr.isSessionSupported('immersive-ar')
+			// .then(function (supported) {
+			// 	if (supported) {
+			// 		const option = new UIRow();
+			// 		option.setClass('option');
+			// 		option.setTextContent('AR');
+			// 		option.onClick(function () {
+			// 			signals.enterXR.dispatch('immersive-ar');
+			// 		});
+			// 		options.add(option);
+			// 	} else {
+			// 		navigator.xr.isSessionSupported('immersive-vr')
+			// 		.then(function (supported) {
+			// 			if (supported) {
+			// 				const option = new UIRow();
+			// 				option.setClass('option');
+			// 				option.setTextContent('VR');
+			// 				option.onClick(function () {
+			// 					signals.enterXR.dispatch('immersive-vr');
+			// 				});
+			// 				options.add(option);
+			// 			}
+			// 		});
+			// 	}
+			// });
 
 		}
-
 	}
 
 	return container;
-
 }
 
 export { MenubarView };
